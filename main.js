7e97e47782dd93d96c003a00e2fe90c712366dc0
Giovanni <bestgio@uw.edu>
2019-06-05 20:27:12 -0700

    styling

--- main.js
+++ main.js
@@ -112,7 +112,8 @@ app.post('/suggestions', (req, res) => {
         let sqlRes = "";
         for(i = result.length - 1; i >= 0; i--) {
 
-            sqlRes += "<h4>" + result[i].Name + " | " + result[i].Time + "</h4>";
+            sqlRes += "<h4>" + result[i].Name + "</h4>";
+            sqlRest += "<p>" + result[i].Time + "</p>";
             sqlRes += "<p>" + result[i].Comment + "</p> <hr>"
             
             
